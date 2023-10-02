ed779e2faa9ef1b8aae8d69ec3031408c81dba89
fucker <aaa@sb.com>
2023-10-02 17:47:03 +0800

    disable luasnip tab key

--- lua/plugins/code/luasnip.lua
+++ lua/plugins/code/luasnip.lua
@@ -17,14 +17,14 @@ return {
     },
     -- stylua: ignore
     keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true, silent = true, mode = "i",
-      },
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+      -- {
+      --   "<tab>",
+      --   function()
+      --     return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+      --   end,
+      --   expr = true, silent = true, mode = "i",
+      -- },
+      -- { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
+      -- { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
 }
