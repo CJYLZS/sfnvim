4a7ac8ef8293cd8609034b2c98ff310bd1bcb998
CJYLZS <691086891@qq.com>
2024-03-15 17:10:36 +0800

    update telescope recentFile

--- lua/plugins/utils/telescope.lua
+++ lua/plugins/utils/telescope.lua
@@ -1,4 +1,52 @@
 -- fuzzy search
+local function recentFiles()
+    local pickers = require("telescope.pickers")
+    local finders = require("telescope.finders")
+    local conf = require("telescope.config").values
+    local make_entry = require("telescope.make_entry")
+
+    local current_buffer = vim.api.nvim_get_current_buf()
+    local current_file = vim.api.nvim_buf_get_name(current_buffer)
+    local results = {}
+
+    for _, buffer in ipairs(vim.split(vim.fn.execute(":buffers! t"), "\n")) do
+        local match = tonumber(string.match(buffer, "%s*(%d+)"))
+        local open_by_lsp = string.match(buffer, "line 0$")
+        if match and not open_by_lsp then
+            local file = vim.api.nvim_buf_get_name(match)
+            if vim.loop.fs_stat(file) and match ~= current_buffer then
+                table.insert(results, file)
+            end
+        end
+        if #results == 15 then
+            break
+        end
+    end
+
+    for _, file in ipairs(vim.v.oldfiles) do
+        local file_stat = vim.loop.fs_stat(file)
+        if file_stat and file_stat.type == "file" and not vim.tbl_contains(results, file) and file ~= current_file then
+            table.insert(results, file)
+        end
+        if #results == 25 then
+            break
+        end
+    end
+
+    pickers
+        .new({}, {
+            prompt_title = "RecentFiles",
+            __locations_input = true,
+            finder = finders.new_table({
+                results = results,
+                entry_maker = make_entry.gen_from_file({}),
+            }),
+            sorter = conf.file_sorter({}),
+            previewer = conf.grep_previewer({}),
+        })
+        :find()
+end
+
 return {
     "nvim-telescope/telescope.nvim",
     -- tag = '0.1.1',
@@ -7,7 +55,7 @@ return {
     keys = {
         { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
         { "<leader>/", "<cmd>Telescope live_grep<CR>", desc = "live grep" },
-        { "<leader>r", "<cmd>Telescope oldfiles<CR>", desc = "Recent" },
+        { "<leader>r", recentFiles, desc = "Recent" },
         { "<leader>f", ":Telescope find_files <CR>", desc = "find files" },
     },
     dependencies = { "nvim-lua/plenary.nvim" },
